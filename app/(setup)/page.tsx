832a2d610b6cd6a7ef291845fb366cc477424991
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-09-15 23:10:30 +0200

    initial profile setup

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+
+export default async function Setup() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) return redirect(`/servers/${server.id}`);
+  return <div>Setup</div>;
+}
